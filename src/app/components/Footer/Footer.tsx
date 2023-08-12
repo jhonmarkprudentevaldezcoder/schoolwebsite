3cc67623bd8683c5986d612f4249689497047f85
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-08-12 20:29:11 +0800

    update

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -9,6 +9,7 @@ import {
   AiFillTwitterSquare,
 } from "react-icons/ai";
 import { BiLogoGmail } from "react-icons/bi";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 export const Footer = () => {
   const [scrolled, setScrolled] = useState(false);
@@ -143,22 +144,9 @@ export const Footer = () => {
             onClick={scrollToTop}
             className={`fixed bottom-4 right-4 ${
               scrolled ? "block" : "hidden"
-            } bg-green-500 text-white rounded-full p-2 hover:bg-green-600 transition-all duration-300`}
+            } bg-green-500 text-white rounded-full p-2  hover:bg-green-600 transition-all duration-300`}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 10l7-7m0 0l7 7m-7-7v18"
-              />
-            </svg>
+            <BsFillArrowUpCircleFill className="text-2xl" />
           </button>
         </div>
       </div>
