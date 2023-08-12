9774a361fc7e25f7d651de1a8bd2ad67c1067b84
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-08-12 19:57:17 +0800

    update

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,40 +1,25 @@
+import Link from "next/link";
 import React from "react";
+import { HiAcademicCap } from "react-icons/hi";
 
 export const Footer = () => {
   return (
     <footer className="bg-green-600">
       <div className="mx-auto w-full max-w-screen-xl">
-        <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
-          <div>
-            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase ">
-              Company
+        <div className="grid grid-cols-1 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
+          <div className="p-5 md:p-0">
+            <h2 className="mb-6 text-sm font-semibold text-white uppercase flex flex-row items-center gap-3 ">
+              <HiAcademicCap className="icons text-4xl" /> ACADEMICS
             </h2>
-            <ul className="text-gray-500  font-medium">
-              <li className="mb-4">
-                <a href="#" className=" hover:underline">
-                  About
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Careers
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Brand Center
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Blog
-                </a>
-              </li>
-            </ul>
+            <p className="text-white">
+              Praesent vel rutrum purus. Nam vel dui eu sus duis dignissim
+              dignissim. Suspenetey disse at ros tecongueconsequat.Fusce sit
+              amet rna feugiat.
+            </p>
           </div>
           <div>
             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-              Help center
+              Featured Links
             </h2>
             <ul className="text-gray-500  font-medium">
               <li className="mb-4">
@@ -61,7 +46,7 @@ export const Footer = () => {
           </div>
           <div>
             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase ">
-              Legal
+              Information
             </h2>
             <ul className="text-gray-500 font-medium">
               <li className="mb-4">
@@ -83,7 +68,7 @@ export const Footer = () => {
           </div>
           <div>
             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase ">
-              Download
+              Newsletter
             </h2>
             <ul className="text-gray-500  font-medium">
               <li className="mb-4">
@@ -110,12 +95,13 @@ export const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="px-4 py-3 w-100 bg-green-700  md:flex md:items-center md:justify-between">
+      <div className="px-4 py-4 w-100 bg-green-700  md:flex md:items-center md:justify-around">
         <span className="text-sm text-white  sm:text-center">
-          © 2023 - 2023. All Rights Reserved.
+          © 2023 - 2023. All Rights Reserved.{" "}
+          <span className="text-white ml-16"> geronimo</span>
         </span>
-        <div className="flex mt-4 space-x-5 sm:justify-center md:mt-0">
-          <a href="#" className="text-gray-400 hover:text-amber-400">
+        <div className="flex mt-4 space-x-5    justify-center md:mt-0">
+          <Link href="#" className="text-white  hover:text-amber-400">
             <svg
               className="w-4 h-4"
               aria-hidden="true"
@@ -124,15 +110,15 @@ export const Footer = () => {
               viewBox="0 0 8 19"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
             <span className="sr-only">Facebook page</span>
-          </a>
+          </Link>
 
-          <a href="#" className="text-gray-400 hover:text-amber-400">
+          <Link href="#" className="text-white hover:text-amber-400">
             <svg
               className="w-4 h-4"
               aria-hidden="true"
@@ -141,15 +127,15 @@ export const Footer = () => {
               viewBox="0 0 20 17"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
             <span className="sr-only">Twitter page</span>
-          </a>
+          </Link>
 
-          <a href="#" className="text-gray-400 hover:text-amber-400">
+          <Link href="#" className="text-white hover:text-amber-400">
             <svg
               className="w-4 h-4"
               aria-hidden="true"
@@ -158,13 +144,13 @@ export const Footer = () => {
               viewBox="0 0 20 20"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
             <span className="sr-only">Dribbble account</span>
-          </a>
+          </Link>
         </div>
       </div>
     </footer>
