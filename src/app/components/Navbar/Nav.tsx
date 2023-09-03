791c5294044653fee5c3fe44c5a779cbaba4b577
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-09-03 21:14:35 +0800

    update

--- src/app/components/Navbar/Nav.tsx
+++ src/app/components/Navbar/Nav.tsx
@@ -28,23 +28,23 @@ export const Nav = () => {
         <div
           className={`${
             !scrolledFromTop ? "h-8" : "h-8 top-0"
-          } bg-red-800 text-gray-300 flex flex-row justify-around `}
+          } bg-gray-200 text-slate-900 flex flex-row justify-around `}
         >
-          <div className="ml-8  flex flex-row items-center gap-1 text-sm">
+          <div className="ml-8  flex flex-row items-center gap-1 text-sm font-bold">
             <MdEmail className="icons" /> wawaes.109480@deped.gov.ph
           </div>
-          <div className="mr-8 mt-1 flex flex-row items-center gap-1 text-sm">
+          <div className="mr-8 mt-1 flex flex-row items-center gap-1 text-sm font-bold ">
             <AiFillPhone className="icons" /> +63123456723
           </div>
         </div>
         <header
-          className={`fixed w-full bg-red-700 flex md:justify-center md:gap-9 justify-between items-center px-4 md:px-12 transition-all duration-200 ${
+          className={`fixed w-full bg-gray-100 flex md:justify-center md:gap-9 justify-between items-center px-4 md:px-12 transition-all duration-200 ${
             !scrolledFromTop ? "h-20 " : "h-14 top-0 shadow-lg"
           }`}
         >
           <Link href="#header">
             <p
-              className={`h-12 transform origin-left mt-4 transition md:mr-10 duration-200 text-xl text-white ${
+              className={`h-12 transform origin-left mt-4 transition md:mr-10 duration-200 text-xl text-slate-900 ${
                 !scrolledFromTop ? "scale-75" : "scale-100 "
               }`}
             >
@@ -55,7 +55,7 @@ export const Nav = () => {
             <button className="md:hidden" onClick={toggleNav}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-8 w-8 text-white"
+                className="h-8 w-8 text-gray-900"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -69,13 +69,13 @@ export const Nav = () => {
               </svg>
             </button>
             <ul
-              className={`fixed left-0 right-0 min-h-screen px-4 pt-8 space-y-4 bg-red-700 text-white transform transition duration-300 ${
+              className={`fixed left-0 right-0 min-h-screen px-4 pt-8 space-y-4 bg-gray-200 text-white transform transition duration-300 ${
                 !navOpen
                   ? "translate-x-full"
                   : "translate-x-0 flex flex-col gap-3"
               } md:relative md:flex  md:space-x-10 md:min-h-0 md:px-0 md:py-0 md:space-y-0 md:translate-x-0`}
             >
-              <li className="w-100 md:bg-red-700 bg-red-800 p-3">
+              <li className="w-100 md:bg-gray-100 bg-gray-100 p-3">
                 <Link
                   className="links"
                   href="#header"
@@ -84,7 +84,7 @@ export const Nav = () => {
                   Home
                 </Link>
               </li>
-              <li className=" md:bg-red-700 w-100 bg-red-800 p-3">
+              <li className=" md:bg-gray-100 w-100 bg-gray-100 p-3">
                 <Link
                   className="links"
                   href="#header"
@@ -93,7 +93,7 @@ export const Nav = () => {
                   Features
                 </Link>
               </li>
-              <li className="md:bg-red-700 w-100 bg-red-800 p-3">
+              <li className="md:bg-gray-100 w-100 bg-gray-100 p-3">
                 <Link
                   className="links"
                   href="#header"
@@ -102,7 +102,7 @@ export const Nav = () => {
                   About
                 </Link>
               </li>
-              <li className="md:bg-red-700 w-100 bg-red-800 p-3">
+              <li className="md:bg-gray-100 w-100 bg-gray-100 p-3">
                 <Link
                   className="links"
                   href="#header"
