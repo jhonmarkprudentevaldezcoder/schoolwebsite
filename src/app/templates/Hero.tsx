ab81a1c5e2387e57ac2900999b4c9210a4f9f8ca
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-09-04 13:03:52 +0800

    updat

--- src/app/templates/Hero.tsx
+++ src/app/templates/Hero.tsx
@@ -5,16 +5,16 @@ export default function Hero() {
   return (
     <div
       className="w-full bg-center bg-cover overflow-hidden bg-no-repeat"
-      style={{
+      /*  style={{
         backgroundImage: "url('/images/bgmatatag.jpg')",
         height: "500px",
         backgroundPosition: "50%",
-      }}
+      }} */
     >
       <div className="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50 py-12">
         <div className="text-center">
           <div className="container px-4 mx-auto">
-            <div className="max-w-4xl mx-auto text-center h-96  "></div>
+            <div className="max-w-4xl mx-auto text-center h-80  "></div>
           </div>
         </div>
       </div>
