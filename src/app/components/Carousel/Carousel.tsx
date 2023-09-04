c0014fa2db368f31e3312ee42341adb9e7acec40
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-09-04 15:48:29 +0800

    update

---
+++ src/app/components/Carousel/Carousel.tsx
@@ -0,0 +1,104 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import "swiper/css";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper/modules";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/effect-fade";
+
+SwiperCore.use([Autoplay]);
+
+export default function Carousel() {
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      autoplay={{
+        delay: 3000, // Delay in milliseconds between each slide
+        disableOnInteraction: false, // Autoplay will not stop when user interacts with Swiper
+      }}
+    >
+      <SwiperSlide>
+        <div
+          className="w-full bg-center bg-cover overflow-hidden bg-no-repeat"
+          style={{
+            backgroundImage: "url('/images/bgmatatag.jpg')",
+            height: "450px",
+            backgroundPosition: "50%",
+          }}
+        >
+          <div className="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50 py-12">
+            <div className="text-center">
+              <div className="container px-4 mx-auto">
+                <div className="max-w-4xl mx-auto text-center h-80  "></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div
+          className="w-full bg-center bg-cover overflow-hidden bg-no-repeat"
+          style={{
+            backgroundImage: "url('/images/bgmatatag.jpg')",
+            height: "450px",
+            backgroundPosition: "50%",
+          }}
+        >
+          <div className="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50 py-12">
+            <div className="text-center">
+              <div className="container px-4 mx-auto">
+                <div className="max-w-4xl mx-auto text-center h-80  "></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div
+          className="w-full bg-center bg-cover overflow-hidden bg-no-repeat"
+          style={{
+            backgroundImage: "url('/images/bgmatatag.jpg')",
+            height: "450px",
+            backgroundPosition: "50%",
+          }}
+        >
+          <div className="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50 py-12">
+            <div className="text-center">
+              <div className="container px-4 mx-auto">
+                <div className="max-w-4xl mx-auto text-center h-80  "></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div
+          className="w-full bg-center bg-cover overflow-hidden bg-no-repeat"
+          style={{
+            backgroundImage: "url('/images/bgmatatag.jpg')",
+            height: "450px",
+            backgroundPosition: "50%",
+          }}
+        >
+          <div className="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50 py-12">
+            <div className="text-center">
+              <div className="container px-4 mx-auto">
+                <div className="max-w-4xl mx-auto text-center h-80  "></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      ...
+    </Swiper>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Carousel from "./components/Carousel/Carousel";
 import About from "./templates/About";
 import Hero from "./templates/Hero";
 import Welcome from "./templates/Welcome";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div className="bg-white ">
       <Hero />
+      <Carousel />
       <Welcome />
       <About />
     </div>
